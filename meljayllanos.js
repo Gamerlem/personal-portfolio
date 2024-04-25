064dd448765c884119e75037099c12ad4ea92d83
Mel Jay Llanos <mel.jay.llanos@gmail.com>
2024-04-25 08:31:25 +0800

    added nav active feature and url change state

--- meljayllanos.js
+++ meljayllanos.js
@@ -3,32 +3,77 @@ const header = document.getElementsByTagName("header");
 const navbar = document.querySelector(".nav-bar");
 const navbarLinks = document.querySelectorAll(".nav-links li a");
 const hamburger = document.querySelector(".hamburger");
+const hamburgerIcon = document.querySelector(".hamburger i");
+const sections = document.querySelectorAll("section");
 
-window.onscroll = updateHeaderBackground;
-hamburger.onclick = showHamburgerNavbarMenu;
+// scroll effect
+window.onscroll = onScrollEffect;
+function onScrollEffect() {
+    // for header 
+    const currentTop = window.scrollY;
+    const headerOffset = Math.ceil(window.innerHeight * 0.11);
+    const scrollOffset = Math.ceil(window.innerHeight * 0.01);
 
-function updateHeaderBackground() {
-    if (window.scrollY >= 60 || navbar.classList.contains('active')) {
-        if(!header[0].classList.contains('colored')) {
-            header[0].classList.add('colored');
+    if (currentTop >= headerOffset || navbar.classList.contains("active")) {
+        if(!header[0].classList.contains("colored")) {
+            header[0].classList.add("colored");
 
             navbarLinks.forEach((link) => {
-                if(!link.classList.contains('colored')) {
-                    link.classList.add('colored');
+                if(!link.classList.contains("colored")) {
+                    link.classList.add("colored");
                 }
             })
         }
     } else {
-        header[0].classList.remove('colored');
+        header[0].classList.remove("colored");
         navbarLinks.forEach((link) => {
-            link.classList.remove('colored');
+            link.classList.remove("colored");
         })
     }
+
+    // for header and section interaction
+    sections.forEach((section) => {
+        const sectionId = section.getAttribute("id");
+        const sectionOffset = section.offsetTop - headerOffset;
+        const sectionHeight = sectionOffset + (section.offsetHeight - scrollOffset);
+
+        if(currentTop >= sectionOffset && currentTop <= sectionHeight) {
+            // update header links
+            navbarLinks.forEach((link) => {
+                link.classList.remove("active");
+            });
+
+            const activeNavbarLink = document.querySelector(".nav-links li a[href*=" + sectionId +"]");
+            if(activeNavbarLink) {
+                activeNavbarLink.classList.add("active");
+            }
+
+            // update current url
+            const location = window.location.toString().split("#")[0];
+            const newLocation = location + "#" + sectionId;
+            const targetLocation = activeNavbarLink ? newLocation : location;
+
+            if(history && history.replaceState) {
+                if(window.location.toString() !== targetLocation) {
+                    history.replaceState(null, null, targetLocation);
+                }
+            }
+        }
+    });
 }
 
+// hamburger menu action
+hamburger.onclick = showHamburgerNavbarMenu;
 function showHamburgerNavbarMenu () {
     navbar.classList.toggle("active");
-    updateHeaderBackground();
+
+    if(navbar.classList.contains("active")) {
+        hamburgerIcon.classList = "bx bx-x";
+    } else {
+        hamburgerIcon.classList = "bx bx-menu";
+    }
+
+    onScrollEffect();
 }
 // #endregion
 
@@ -47,10 +92,10 @@ const minuteTextElement = document.getElementById("stats-minutes-text");
 const secondElement = document.getElementById("stats-seconds");
 const secondTextElement = document.getElementById("stats-seconds-text");
 
-// set start
-const startDate = new Date("June 14, 2022").getTime();
 // set default values
+const startDate = new Date("June 14, 2022").getTime();
 calculateProfessionalTime();
+
 // update each 1ms
 const timer = setInterval(calculateProfessionalTime, 1000);
 
@@ -102,5 +147,16 @@ function openInfo(event, infoName) {
     // // Show the current tab, and add an "active" class to the button that opened the tab
     event.currentTarget.className += " active";
     document.getElementById(infoName).className += " active";
+}
+
+function viewResume() {
+    window.open("files/Mel Jay Llanos - Resume.pdf", "_blank");
+}
+// #endregion
+
+
+// #region
+function sendEmail() {
+    window.location.href = "mailto:mel.jay.llanos@gmail.com";
 }
 // #endregion
